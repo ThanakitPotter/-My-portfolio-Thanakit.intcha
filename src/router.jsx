f60f787cd419558080c588f08cc8576592f5aebf
ThanakitPotter <Thanakit.intcha@gmail.com>
2025-11-24 14:21:58 +0700

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -4,21 +4,21 @@ import { lazy } from "react";
 import RootLayout from "./layouts/RootLayout.jsx";
 
 
-const Home = lazy(() => import("./pages/Home.jsx"));       // แก้ Home -> home
-const About = lazy(() => import("./pages/about.jsx"));     // แก้ About -> about
-const Projects = lazy(() => import("./pages/projects.jsx")); // แก้ Projects -> projects
-const Strength = lazy(() => import("./pages/Strength.jsx")); // แก้ Strength -> strength
-const Contact = lazy(() => import("./pages/contact.jsx"));   // แก้ Contact -> contact
+const Home = lazy(() => import("./pages/Home.jsx"));       
+const About = lazy(() => import("./pages/about.jsx"));     
+const Projects = lazy(() => import("./pages/projects.jsx")); 
+const Strength = lazy(() => import("./pages/Strength.jsx")); 
+const Contact = lazy(() => import("./pages/contact.jsx"));   
 
 export const router = createBrowserRouter([
   {
     element: <RootLayout />, // Layout หลัก
     children: [
       { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/projects", element: <Projects /> },
-      { path: "/strength", element: <Strength /> },
-      { path: "/contact", element: <Contact /> },
+      { path: "/About", element: <About /> },
+      { path: "/Projects", element: <Projects /> },
+      { path: "/Strength", element: <Strength /> },
+      { path: "/Contact", element: <Contact /> },
     ],
   },
 ]);
